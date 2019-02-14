baf76a44f77a6d577bf5d2493f432e18add4d50c
Aditya Subramanyam <subramanyam963@gmail.com>
2019-02-14 19:30:51 +0530

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = function(api, opts = {}) {
+    return {
+        plugins: [
+            require('@babel/plugin-syntax-jsx'),
+            [
+                require('@babel/plugin-transform-react-jsx'),
+                {
+                    pragma: opts.pragma || 'grape.createVirtualNode'
+                }
+            ]
+        ]
+    };
+}
